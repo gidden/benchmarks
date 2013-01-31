236c6f948676964f66e56b8346564dcd069199ca
gidden <matthew.gidden@gmail.com>
2013-01-31 16:02:39 -0600

    updated nea json file, nea xml file, and json2rng script to correctly read in and write recipes

--- json2rng.py
+++ json2rng.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
@@ -9,16 +11,17 @@ try:
 except ImportError:
     import json
 from lxml import etree
-from pyne import nucame
+from pyne import nucname
 
 
 class CyclusTransformer(object):
     """I'm a little teapot."""
     
-    def __init__(self, filename):
-        with open(filename, 'r') as f:
+    def __init__(self, jfile,rfile):
+        with open(jfile, 'r') as f:
             self.jroot = json.load(f)
         self.xroot = etree.Element('simulation')
+        self.rroot = etree.parse(rfile).getroot()
             
     def __str__(self):
         s = "\n".join(["JSON Root =", '-'*40, pformat(self.jroot), '', ''])
@@ -50,11 +53,45 @@ class CyclusTransformer(object):
         recipename.text = name + '_recipe'
         basis = etree.SubElement(recipe, 'basis')
         basis.text = 'mass'
-        for 
+        if mat['recipe']:
+            self.visit_recipe_material(mat,recipe)
+        else:
+            self.visit_stored_recipe(name,recipe)
+
+    def visit_recipe_material(self,mat,recipe):
+        constraints = mat['constraints']
+        for constraint in constraints:
+            if nucname.isnuclide(constraint[0]):
+                self.visit_nuclide(recipe,constraint[0],constraint[1])
+
+    def visit_stored_recipe(self,name,recipe):
+        nodes = self.iso_nodes(name)
+        for node in nodes:
+            self.visit_nuclide(recipe,node[0].text,node[1].text)
+
+    def visit_nuclide(self,recipe,nuc,val):
+        isotope = etree.SubElement(recipe,'isotope')
+        isoid = etree.SubElement(isotope,'id')
+        isoid.text=str(nuc)
+        isoval = etree.SubElement(isotope,'comp')
+        isoval.text=str(val)
+
+    def iso_nodes(self,name):
+        recipe_names = self.rroot.xpath('recipe/name')
+        for i in range(len(recipe_names)):
+            if recipe_names[i].text == name:
+                return self.rroot.xpath('recipe')[i].findall('isotope')
+        raise RecipeError("No recipe named " + name 
+                          + " found in " + self.rfile)
+
+class RecipeError(Exception):
+    """Exception indicating a Recipe wasn't found."""
+    pass
+
 
 # <codecell>
 
-ct = CyclusTransformer('nea1a.json')
+ct = CyclusTransformer('nea1a.json','nea_recipes.xml')
 ct.visit()
 print ct
 
