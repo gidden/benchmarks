29ebfdaeae82aa02a164a319bab6e8086b15dc8c
gidden <matthew.gidden@gmail.com>
2013-05-14 11:47:37 -0500

    updated testing framework. I now test for object similarity, rather than string similarity. current representation of testing xml and json objects complete, time to write translation function

--- test_matl_translate.py
+++ test_matl_translate.py
@@ -1,51 +1,61 @@
+# local 
 from matl_translate import json_to_xml, xml_to_json
+
+# json/xml packages
+try:
+    import simplejson as json
+except ImportError:
+    import json
+from lxml import etree
+
+# test packages
 from nose.tools import assert_equal
 
+def test_translation():
+    name,ntopes,xml_isotopes,json_isotopes,values = setup_constants()
+    xml_node = setup_xml(name,ntopes,xml_isotopes,values)
+    json_node = setup_json(name,ntopes,json_isotopes,values)
+    assert_equal(xml_node,json_to_xml(json_node))
+    #assert_equal(json_str,xml_to_json(xml_str))
+
 def setup_xml(name,ntopes,isotopes,values):
-    xml_str = \
-        "<recipe>" + \
-        "<name>"+name+"</name>" +\
-        "<basis>mass</basis>"
-
+    root = etree.Element("recipe")
+    elname = etree.SubElement(root,"name")
+    elname.text = name
+    elbasis = etree.SubElement(root,"basis")
+    elbasis.text = "mass"
     for i in range(ntopes):
-        xml_str += \
-            "<isotope>" +\
-            "<id>"+isotopes[i]+"</id>" +\
-            "<comp>"+values[i]+"</comp>" +\
-            "</isotope>"
-
-    xml_str += "</recipe>"
-    return xml_str
+        eliso = etree.SubElement(root,"isotope")
+        elid = etree.SubElement(eliso,"id")
+        elid.text = isotopes[i]
+        elval = etree.SubElement(eliso,"comp")
+        elval.text = str(values[i])
+    return root
 
 def setup_json(name,ntopes,isotopes,values):
-    json_str = \
-        "\""+name+"\": {" +\
-	"\"recipe\": true," +\
-	"\"constraints\": ["
-
+    jisotopes = []
     for i in range(ntopes):
-        json_str += "[\""+isotopes[i]+"\", "+values[i]+"]"
-        if (i < ntopes-1):
-            json_str += ","
-        else:
-            json_str += "]"
+        jisotopes.append([isotopes[i],values[i]])
+    obj = {name:{\
+               "attributes":{"recipe":"true"},\
+               "constraints":jisotopes\
+          }}
+    return json.dumps(obj, indent = 4 * ' ')
 
-    json_str += "}"
-    return json_str
-
-def test_translation():
+def setup_constants():
     #constants
     name = "a_name"
     ntopes = 2
     xml_isotopes = ["92235", "92238"]
     json_isotopes = ["U235", "U238"]
-    values = ["1e0", "2e0"]
+    values = [1e0, 2e0]
+    return name,ntopes,xml_isotopes,json_isotopes,values
 
-    xml_str = setup_xml(name,ntopes,xml_isotopes,values)
-    json_str = setup_json(name,ntopes,json_isotopes,values)
+if __name__ == "__main__":
+    name,ntopes,xml_isotopes,json_isotopes,values = \
+        setup_constants()
+    xml_node = setup_xml(name,ntopes,xml_isotopes,values)
+    json_node = setup_json(name,ntopes,json_isotopes,values)
+    print etree.tostring(xml_node)
+    print json_node
     
-    xml_str = "hi"
-    json_str = "hi"
-    assert_equal(xml_str,json_to_xml(json_str))
-    assert_equal(json_str,xml_to_json(xml_str))
-
