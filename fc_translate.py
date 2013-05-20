5aa7bef8c82102882aa82f9e0cb6b63df73c0246
gidden <matthew.gidden@gmail.com>
2013-05-20 17:51:50 -0500

    control block (sim info) content added, passes tests
    testing function signature take a single object per type of test, default
    objects included.

--- fc_translate.py
+++ fc_translate.py
@@ -9,6 +9,16 @@ class CyclusFuelCycle(object):
         self.initial_conditions = initial_conditions
         self.growth = growth
 
+class SimulationDefaults(object):
+    """ A holding class to be a singular point to define all default parameters
+    used in the translation.
+    """
+    def __init__(self):
+        self.startmonth = "1"
+        self.startyear = "2000"
+        self.simstart = "0"
+        self.decay = "2"
+
 class JsonFuelCycleParser(object):
     """ A parser that accepts a python-based json object representation of
     the fuel cycle from the FCS benchmark specification language and returns a
@@ -16,6 +26,37 @@ class JsonFuelCycleParser(object):
     """
     def __init__(self, description):
         self.__description = description
+
+    def __duration(self):
+        units = self.__description["attributes"]["grid"]
+        time = self.__description["constraints"]["grid"]
+        diff = time[1] - time[0]
+        if units is "years":
+            diff *= 12
+        return diff
+
+    def __constructSimInfo(self):
+        root = etree.Element("control")
+        duration = etree.SubElement(root,"duration")
+        duration.text = str(self.__duration())
+        defaults = SimulationDefaults()
+        startmonth = etree.SubElement(root,"startmonth")
+        startmonth.text = defaults.startmonth
+        startyear = etree.SubElement(root,"startyear")
+        startyear.text = defaults.startyear
+        simstart = etree.SubElement(root,"simstart")
+        simstart.text = defaults.simstart
+        decay = etree.SubElement(root,"decay")
+        decay.text = defaults.decay
+        return root
+
+    def __constructInitialCondition(self):
+        ics = []
+        return ics
+
+    def __constructGrowth(self):
+        demands = []
+        return demands
 
     def parse(self):
         """ Takes as input a python dictionary of the fuel cycle as specified in
@@ -26,15 +67,3 @@ class JsonFuelCycleParser(object):
         ics = self.__constructInitialCondition()
         growth = self.__constructGrowth()
         return CyclusFuelCycle(info, ics, growth)
-
-    def __constructSimInfo(self):
-        root = etree.Element("simulation")
-        return root
-
-    def __constructInitialCondition(self):
-        ics = []
-        return ics
-
-    def __constructGrowth(self):
-        demands = []
-        return demands
--- test_fc_translate.py
+++ test_fc_translate.py
@@ -11,16 +11,78 @@ from lxml import etree
 # test packages
 from nose.tools import assert_equal, assert_raises
 
-def test_everything():
-    # expected
-    info_xml = etree.Element("simulation")
-    ics = []
-    growth = []
-    
+
+ # "fuelCycle": {
+ #     "attributes": {
+ #         "grid": "year",
+ #         "initialConditions": {
+ #             "repository": 1,
+ #         },
+ #         "demands": {
+ #             "power": ["GWe", ["lwrReactor"]]
+ #         }
+ #     }
+ #     "constraints": {
+ #         "grid": [0, 120],
+ #         "demands": {
+ #             "power": {
+ #                 "grid": [0,120],
+ #                 "growth": {
+ #        	     "type": "linear",
+ #        	     "period": {
+ #        	         "startTime": 0,
+ #        	         "startValue": 1000,
+ #                         "slope": 500
+ #        	     }
+ #                 }
+ #             }
+ #         }
+ #     }
+ # }
+
+def fill_info(attr, units, constrs, time):
+    attr["grid"] = units
+    constrs["grid"] = time
+
+def get_description(time_units, time):
+    obj = {}
+    obj["attributes"] = {}
+    obj["constraints"] = {}
+    fill_info(obj["attributes"],time_units,obj["constraints"],time)
+    return obj
+
+def get_info_xml(time_units,time):
+    diff = time[1] - time[0]
+    if time_units is "years":
+        diff *= 12
+    root = etree.Element("control")
+    duration = etree.SubElement(root,"duration")
+    duration.text = str(diff)
+    startmonth = etree.SubElement(root,"startmonth")
+    startmonth.text = "1" # default starting month
+    startyear = etree.SubElement(root,"startyear")
+    startyear.text = "2000" # default starting year
+    simstart = etree.SubElement(root,"simstart")
+    simstart.text = "0" # default starting time period
+    decay = etree.SubElement(root,"decay")
+    decay.text = "2" # default decay switch
+    return root
+
+def runtests(time_vars):
+    # variables
+    time_units = time_vars[0]
+    time = time_vars[1]
+
     # observed
-    description = {}
+    description = get_description(time_units,time)
     parser = JsonFuelCycleParser(description)
     fc = parser.parse()
+
+    # expected
+    info_xml = get_info_xml(time_units,time)
+    #print "\n" + etree.tostring(info_xml, pretty_print = True)
+    ics = []
+    growth = []
 
     # tests
     assert_equal(etree.tostring(info_xml), etree.tostring(fc.info))
@@ -28,3 +90,19 @@ def test_everything():
         assert_equal(etree.tostring(ics[i]), etree.tostring(fc.ics[i]))
     for i in range(len(growth)):
         assert_equal(etree.tostring(growth[i]), etree.tostring(fc.growth[i]))
+
+def default_time_vars():
+    return ["years", [1,100]]
+
+def test_default():
+    runtests(default_time_vars())
+
+def test_years():
+    time_units = "years"
+    time = [1,1180]
+    runtests([time_units, time])
+
+def test_month():
+    time_units = "months"
+    time = [1, 1180]
+    runtests([time_units, time])
